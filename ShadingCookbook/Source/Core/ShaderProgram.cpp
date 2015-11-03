c869913b6371249ddd51e7eebbb7728ac391b25a
nameless323 <nameless323@gmail.com>
2015-11-03 15:09:04 +0300

    shader ext

--- ShadingCookbook/Source/Core/ShaderProgram.cpp
+++ ShadingCookbook/Source/Core/ShaderProgram.cpp
@@ -1,1 +1,31 @@
 #include "ShaderProgram.h"
+
+#include <fstream>
+using std::ifstream;
+using std::ios;
+
+#include <sstream>
+#include <sys/stat.h>
+
+namespace ShaderInfo
+{
+	struct ShaderFileExtension
+	{
+		const char* ext;
+		Shaders::ShaderType type;
+	};
+
+	struct ShaderFileExtension Extensions[]
+	{
+		{".vs", Shaders::VERTEX},
+		{".vert", Shaders::VERTEX},
+		{".gs", Shaders::GEOMETRY},
+		{".geom", Shaders::GEOMETRY},
+		{".tcs", Shaders::TESS_CONTROL},
+		{".tes", Shaders::TESS_EVALUATION},
+		{".fs", Shaders::FRAGMENT},
+		{".frag", Shaders::FRAGMENT},
+		{".cs", Shaders::COMPUTE}
+	};
+}
+
