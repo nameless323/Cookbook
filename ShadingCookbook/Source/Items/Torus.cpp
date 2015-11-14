654f1d3396e141a5f648fd4b206720e8d641f386
nameless323 <nameless323@gmail.com>
2015-11-14 16:45:39 +0300

    torus magic fix

--- ShadingCookbook/Source/Items/Torus.cpp
+++ ShadingCookbook/Source/Items/Torus.cpp
@@ -10,58 +10,52 @@ Torus::Torus(float outerRadius, float innerRadius, int nsides, int nrings) :
 	_rings(nrings), _sides(nsides)
 {
 	_faces = _sides * _rings;
-	int nVerts = _sides * (_rings + 1);   // One extra ring to duplicate first ring
+	int nVerts = _sides * (_rings + 1);
 
-										// Verts
-	float * v = new float[3 * nVerts];
-	// Normals
-	float * n = new float[3 * nVerts];
-	// Tex coords
-	float * tex = new float[2 * nVerts];
-	// Elements
-	unsigned int * el = new unsigned int[6 * _faces];
+	float* v = new float[3 * nVerts];
+	float* n = new float[3 * nVerts];
+	float* uv = new float[2 * nVerts];
 
-	// Generate the vertex data
-	GenerateVerts(v, n, tex, el, outerRadius, innerRadius);
+	unsigned int* el = new unsigned int[6 * _faces];
 
-	// Create and populate the buffer objects
-	unsigned int handle[4];
-	glGenBuffers(4, handle);
+	GenerateVerts(v, n, uv, el, outerRadius, innerRadius);
 
-	glBindBuffer(GL_ARRAY_BUFFER, handle[0]);
+	unsigned int buffersHandle[4];
+
+	glGenBuffers(4, buffersHandle);
+
+	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle[0]);
 	glBufferData(GL_ARRAY_BUFFER, (3 * nVerts) * sizeof(float), v, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ARRAY_BUFFER, handle[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle[1]);
 	glBufferData(GL_ARRAY_BUFFER, (3 * nVerts) * sizeof(float), n, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ARRAY_BUFFER, handle[2]);
-	glBufferData(GL_ARRAY_BUFFER, (2 * nVerts) * sizeof(float), tex, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, handle[3]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * _faces * sizeof(unsigned int), el, GL_STATIC_DRAW);
-
+	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle[2]);
+	glBufferData(GL_ARRAY_BUFFER, (2 * nVerts)*sizeof(float), uv, GL_STATIC_DRAW);
+		
 	delete[] v;
 	delete[] n;
-	delete[] el;
-	delete[] tex;
+	delete[] uv;
 
-	// Create the VAO
 	glGenVertexArrays(1, &_vaoHandle);
 	glBindVertexArray(_vaoHandle);
 
-	glEnableVertexAttribArray(0);  // Vertex position
-	glBindBuffer(GL_ARRAY_BUFFER, handle[0]);
-	glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, ((GLubyte *)NULL + (0)));
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-	glEnableVertexAttribArray(1);  // Vertex normal
-	glBindBuffer(GL_ARRAY_BUFFER, handle[1]);
-	glVertexAttribPointer((GLuint)1, 3, GL_FLOAT, GL_FALSE, 0, ((GLubyte *)NULL + (0)));
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle[1]);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-	glBindBuffer(GL_ARRAY_BUFFER, handle[2]);
-	glEnableVertexAttribArray(2);  // Texture coords
-	glVertexAttribPointer((GLuint)2, 2, GL_FLOAT, GL_FALSE, 0, ((GLubyte *)NULL + (0)));
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle[2]);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, handle[3]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffersHandle[3]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * _faces * sizeof(unsigned int), el, GL_STATIC_DRAW);
+
+	delete[] el;
 
 	glBindVertexArray(0);
 }
@@ -71,62 +65,58 @@ void Torus::Render()  {
 	glDrawElements(GL_TRIANGLES, 6 * _faces, GL_UNSIGNED_INT, ((GLubyte *)NULL + (0)));
 }
 
-void Torus::GenerateVerts(float * verts, float * norms, float * tex,
-	unsigned int * el,
-	float outerRadius, float innerRadius)
+void Torus::GenerateVerts(float* verts, float* norms, float* tex, unsigned* el, float outerRadius, float innerRadius)
 {
 	float ringFactor = glm::two_pi<float>() / _rings;
 	float sideFactor = glm::two_pi<float>() / _sides;
 	int idx = 0, tidx = 0;
-	for (int ring = 0; ring <= _rings; ring++) {
-		float u = ring * ringFactor;
-		float cu = cos(u);
-		float su = sin(u);
-		for (int side = 0; side < _sides; side++) {
+	for (int ring = 0; ring <= _rings; ring++)
+	{
+		float u = ring*ringFactor;
+		float cu = cosf(u);
+		float su = sinf(u);
+		for (int side = 0; side < _sides; side++)
+		{
 			float v = side * sideFactor;
-			float cv = cos(v);
-			float sv = sin(v);
-			float r = (outerRadius + innerRadius * cv);
+			float cv = cosf(v);
+			float sv = sinf(v);
+			float r = (outerRadius + innerRadius*cv);
 			verts[idx] = r * cu;
 			verts[idx + 1] = r * su;
-			verts[idx + 2] = innerRadius * sv;
-			norms[idx] = cv * cu * r;
+			verts[idx + 2] = innerRadius*sv;
+
+			norms[idx] = cv * su * r;
 			norms[idx + 1] = cv * su * r;
 			norms[idx + 2] = sv * r;
+
 			tex[tidx] = u / glm::two_pi<float>();
 			tex[tidx + 1] = v / glm::two_pi<float>();
 			tidx += 2;
-			// Normalize
-			float len = sqrt(norms[idx] * norms[idx] +
-				norms[idx + 1] * norms[idx + 1] +
-				norms[idx + 2] * norms[idx + 2]);
+			float len = sqrt(norms[idx] * norms[idx] + norms[idx + 1] * norms[idx + 1] + norms[idx + 2] * norms[idx + 2]);
 			norms[idx] /= len;
 			norms[idx + 1] /= len;
 			norms[idx + 2] /= len;
 			idx += 3;
 		}
 	}
-
 	idx = 0;
-	for (int ring = 0; ring < _rings; ring++) {
-		int ringStart = ring * _sides;
+	for (int ring = 0; ring < _rings; ring++)
+	{
+		int ringStart = ring*_sides;
 		int nextRingStart = (ring + 1) * _sides;
-		for (int side = 0; side < _sides; side++) {
+		for (int side = 0; side < _sides; side++)
+		{
 			int nextSide = (side + 1) % _sides;
-			// The quad
 			el[idx] = (ringStart + side);
 			el[idx + 1] = (nextRingStart + side);
 			el[idx + 2] = (nextRingStart + nextSide);
-			el[idx + 3] = ringStart + side;
-			el[idx + 4] = nextRingStart + nextSide;
+			el[idx + 3] = (ringStart + side);
+			el[idx + 4] = (nextRingStart + nextSide);
 			el[idx + 5] = (ringStart + nextSide);
 			idx += 6;
 		}
 	}
-
-
 }
-
 int Torus::GetVertexArrayHandle() {
 	return this->_vaoHandle;
 }
