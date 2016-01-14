96c83547f4d43126850cbfa46dbdddd5538a77c9
nameless323 <nameless323@gmail.com>
2016-01-14 15:42:24 +0300

    tga read

---
+++ ShadingCookbook/Source/Core/TGA.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdexcept>
+#include "GL/glew.h"
+
+
+#include <fstream>
+
+namespace TGA
+{
+	class IOException : public std::runtime_error
+	{
+	public:
+		IOException (const std::string& msg) : std::runtime_error(msg)
+		{}
+	};
+
+	namespace LittleEndian
+	{
+		int ReadShort(std::ifstream& stream);
+		void WriteShort(std::ofstream& stream, int value);
+
+		int ReadInt(std::ifstream& stream);
+	}
+
+	GLubyte* Read(const char* filename, int& width, int& height);
+	void Write(GLubyte* pixelData, int width, int height, const char* filename);
+
+	GLuint LoadTex(const char* filename, GLint& width, GLint& height);
+
+	GLuint LoadTex(const char* filename);
+}
