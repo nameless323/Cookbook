3c7fc9428bbc386512caa19bb7ce06bc53f3b57a
nameless323 <nameless323@gmail.com>
2016-02-04 14:47:30 +0300

    tex discard h and cpp

---
+++ ShadingCookbook/Source/Scene/Chapter4/TexDiscard.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../../Core/ShaderProgram.h"
+#include "../Scene.h"
+#include "../../Items/Teapot.h"
+#include "../../Items/Plane.h"
+#include "../../Items/Torus.h"
+#include "../../Items/Cube.h"
+
+#include <glm.hpp>
+using glm::mat4;
+
+class TexDiscard : public Scene
+{
+public:
+	TexDiscard();
+	void ProcessInput(int key, int action) override;
+	void InitScene() override;
+	void Update(float t);
+	void Render() override;
+	void Shutdown();
+	void Resize(int x, int y);
+private:
+	ShaderProgram _shader;
+	int _width, _height;
+	Teapot* _teapot;
+	Plane* _plane;
+	Cube* _cube;
+	Torus* _torus;
+	mat4 _model;
+	mat4 _view;
+	mat4 _projection;
+	float _angle;
+	float _prevTime;
+	bool _autorotate;
+	bool _rotateLeft;
+	bool _rotateRight;
+
+	void SetMatrices();
+	void CompileAndLinkShader();
+};
