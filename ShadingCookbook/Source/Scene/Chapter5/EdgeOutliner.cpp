5eb27d312eaa57796f0bc5efa504cb195739e557
nameless323 <nameless323@gmail.com>
2016-03-08 16:26:47 +0300

    edge outliner cpp finished

--- ShadingCookbook/Source/Scene/Chapter5/EdgeOutliner.cpp
+++ ShadingCookbook/Source/Scene/Chapter5/EdgeOutliner.cpp
@@ -114,39 +114,34 @@ void EdgeOutliner::SetupFBO()
 	glGenTextures(1, &renderTex);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, renderTex);
-	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, 512, 512);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexStorage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _width, _height);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTex, 0);
 
-	GLuint depthBuf;
-	glGenRenderbuffers(1, &depthBuf);
-	glBindRenderbuffer(GL_RENDERBUFFER, depthBuf);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 512, 512);
+	GLuint depth;
+	glGenRenderbuffers(1, &depth);
+	glBindRenderbuffer(GL_RENDERBUFFER, depth);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, _width, _height);
 
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuf);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth);
 
 	GLenum drawBuffers[] = { GL_COLOR_ATTACHMENT0 };
 	glDrawBuffers(1, drawBuffers);
-
-	GLenum result = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-	if (result == GL_FRAMEBUFFER_COMPLETE)
-		std::cout << "Framebuffer complete" << std::endl;
-	else
-		std::cout << "Framebuffer error: " << result << std::endl;
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void EdgeOutliner::Pass1()
 {
-	_shader.SetUniform("RenderTex", 1);
-	glViewport(0, 0, 512, 512);
+	glBindFramebuffer(GL_FRAMEBUFFER, _fboHandle);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//	_plane->Render();
 
-	_view = glm::lookAt(vec3(0.0f, 0.0f, 7.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
-	_projection = glm::perspective(glm::radians(60.0f), 1.0f, 0.3f, 100.0f);
+	glUniformSubroutinesuiv(GL_FRAGMENT_SHADER, 1, &_pass1Index);
+
+	_view = glm::lookAt(vec3(7.0f * cos(_angle), 4.0f, 7.0f * sin(_angle)), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+	_projection = glm::perspective(glm::radians(60.0f), (float)_width / _height, 0.3f, 100.0f);
+
 	_shader.SetUniform("Light.Position", vec4(0.0f, 0.0f, 0.0f, 1.0f));
 	_shader.SetUniform("Material.Kd", 0.9f, 0.9f, 0.9f);
 	_shader.SetUniform("Material.Ks", 0.95f, 0.95f, 0.95f);
@@ -154,38 +149,53 @@ void EdgeOutliner::Pass1()
 	_shader.SetUniform("Material.Shininess", 100.0f);
 
 	_model = mat4(1.0f);
-	_model = glm::translate(_model, vec3(0.0f, -1.5f, 0.0f));
-	_model = glm::rotate(_model, glm::radians(-90.0f), vec3(1.0f, 0.0f, 0.0f));
+	_model *= glm::translate(vec3(0.0f, 0.0f, 0.0f));
+	_model *= glm::rotate(glm::radians(-90.0f), vec3(1.0f, 0.0f, 0.0f));
 	SetMatrices();
 	_teapot->Render();
-}
 
-void EdgeOutliner::Pass2()
-{
-	_shader.SetUniform("RenderTex", 0);
-	glViewport(0, 0, _width, _height);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	vec3 cameraPos = vec3(2.0f * cos(_angle), 1.5f, 2.0f * sin(_angle));
-	_view = glm::lookAt(cameraPos, vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
-	_projection = glm::perspective(glm::radians(45.0f), (float)_width / _height, 0.3f, 100.0f);
-
-	_shader.SetUniform("Light.Position", vec4(0.0f, 0.0f, 0.0f, 1.0f));
 	_shader.SetUniform("Material.Kd", 0.4f, 0.4f, 0.4f);
 	_shader.SetUniform("Material.Ks", 0.0f, 0.0f, 0.0f);
 	_shader.SetUniform("Material.Ka", 0.1f, 0.1f, 0.1f);
 	_shader.SetUniform("Material.Shininess", 1.0f);
 	_model = mat4(1.0f);
+	_model *= glm::translate(vec3(0.0f, -0.75f, 0.0f));
 	SetMatrices();
-	_cube->Render();
+	_plane->Render();
+
+	_shader.SetUniform("Light.Position", vec4(0.0f, 0.0f, 0.0f, 1.0f));
+	_shader.SetUniform("Material.Kd", 0.9f, 0.5f, 0.2f);
+	_shader.SetUniform("Material.Ks", 0.95f, 0.95f, 0.95f);
+	_shader.SetUniform("Material.Ka", 0.1f, 0.1f, 0.1f);
+	_shader.SetUniform("Material.Shininess", 100.0f);
+	_model = mat4(1.0f);
+	_model *= glm::translate(vec3(1.0f, 1.0f, 3.0f));
+	_model *= glm::rotate(glm::radians(90.0f), vec3(1.0f, 0.0f, 0.0f));
+	SetMatrices();
+	_torus->Render();
+}
+
+void EdgeOutliner::Pass2()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUniformSubroutinesuiv(GL_FRAGMENT_SHADER, 1, &_pass2Index);
+	_model = mat4(1.0f);
+	_view = mat4(1.0f);
+	_projection = mat4(1.0f);
+	SetMatrices();
+	glBindVertexArray(_fsQuad);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
 
 void EdgeOutliner::Shutdown()
 {
 	delete _plane;
-	delete _cube;
+	delete _torus;
 	delete _teapot;
+	//del framebufs and renderbufs
 }
 
 void EdgeOutliner::SetMatrices()
