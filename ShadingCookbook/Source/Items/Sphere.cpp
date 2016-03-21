364706bf8fe3c4167679f5dfa40d7f35c595b658
nameless323 <nameless323@gmail.com>
2016-03-21 14:01:26 +0300

    sphere

--- ShadingCookbook/Source/Items/Sphere.cpp
+++ ShadingCookbook/Source/Items/Sphere.cpp
@@ -1,6 +1,10 @@
 #include "Sphere.h"
 
-inline Sphere::Sphere(float radius, GLuint slices, GLuint stacks) : _radius(radius), _slices(slices), _stacks(_stacks)
+#include <cstdio>
+#include <cmath>
+#include <gtc/constants.hpp>
+
+Sphere::Sphere(float radius, GLuint slices, GLuint stacks) : _radius(radius), _slices(slices), _stacks(_stacks)
 {
 	_nVerts = (slices + 1) * (stacks + 1);
 	_elements = (slices * 2 * (stacks - 1)) * 3;
@@ -65,4 +69,71 @@ int Sphere::GetVAOHandle()
 
 void Sphere::GenerateVerts(float* verts, float* norms, float* tex, unsigned* el)
 {
+
+	GLfloat theta;
+	GLfloat phi;
+	GLfloat thetaFac = glm::two_pi<float>() / _slices;
+	GLfloat phiFac = glm::pi<float>() / _stacks;
+	GLfloat nx, ny, nz, s, t;
+	GLuint idx = 0, tIdx = 0;
+	for (GLuint i = 0; i < _slices; i++)
+	{
+		theta = i * thetaFac;
+		s = (GLfloat)i / _slices;
+		for (GLuint j = 0; j <= _stacks; j++)
+		{
+			phi = j * phiFac;
+			t = (GLfloat)j / _stacks;
+			nx = sinf(phi) * cosf(theta);
+			ny = sinf(phi) * sinf(theta);
+			nz = cosf(phi);
+			verts[idx] = _radius * nx;
+			verts[idx + 1] = _radius * ny;
+			verts[idx + 2] = _radius * nz;
+
+			norms[idx] = nx;
+			norms[idx + 1] = ny;
+			norms[idx + 2] = nz;
+			idx += 2;
+
+			tex[tIdx] = s;
+			tex[tIdx + 1] = t;
+			tIdx += 2;
+
+		}
+	}
+
+	idx = 0;
+	for (GLuint i = 0; i < _slices; i++)
+	{
+		GLuint stackStart = i * (_stacks + 1);
+		GLuint nextStackStart = (i + 1) * (_stacks + 1);
+		for (GLuint j = 0; j < _stacks; j++)
+		{
+			if ( j == 0)
+			{
+				el[idx] = stackStart;
+				el[idx + 1] = stackStart + 1;
+				el[idx + 2] = nextStackStart + 1;
+				idx += 3;
+			}
+			else if (j == (_stacks - 1))
+			{
+				el[idx] = stackStart + j;
+				el[idx + 1] = stackStart + j + 1;
+				el[idx + 2] = nextStackStart + j;
+				idx += 3;
+			}
+			else
+			{
+				el[idx] = stackStart + j;
+				el[idx + 1] = stackStart + j + 1;
+				el[idx + 2] = nextStackStart + j + 1;
+				el[idx + 3] = nextStackStart + j;
+				el[idx + 4] = stackStart + j;
+				el[idx + 5] = nextStackStart + j + 1;
+				idx += 6;
+			}
+		}
+	} 
 }
