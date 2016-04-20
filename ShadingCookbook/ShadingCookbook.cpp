93b9d4263975985d7035be0239ba5c8ffab8635a
nameless323 <nameless323@gmail.com>
2016-04-20 11:07:07 +0300

    deferred shaders

--- ShadingCookbook/ShadingCookbook.cpp
+++ ShadingCookbook/ShadingCookbook.cpp
@@ -75,7 +75,7 @@ void ResizeGL(int w, int h)
 
 int main(void)
 {
-	scene = new Mandelbrot;
+	scene = new Particles;
 
 	if (!glfwInit()) exit(EXIT_FAILURE);
 //	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
