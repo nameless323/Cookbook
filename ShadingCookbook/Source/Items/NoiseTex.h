d3e75318c48e85ed40c1d3cd40a4828df3ae7af9
nameless323 <nameless323@gmail.com>
2016-07-11 10:44:36 +0400

    noise tex

---
+++ ShadingCookbook/Source/Items/NoiseTex.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <GL/glew.h>
+
+namespace NoiseTex
+{
+	int StoreTex (GLubyte* data, int w, int h);
+	int Generate2DTex (float baseFreq = 4.0f, float persistence = 0.5f, int w = 128, int h = 128, bool periodic = false);
+	int GeneratePeriodic2DTex(float baseFreq = 4.0f, float persist = 0.5f, int w = 128, int h = 128);
+}
