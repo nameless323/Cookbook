7fd179b155241bb2a86b821a90742f36d6ee4c02
nameless323 <nameless323@gmail.com>
2016-08-16 11:30:32 +0400

    simple particles

---
+++ ShadingCookbook/Source/Scene/Chapter9/SimpleParticles.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../../Core/ShaderProgram.h"
+#include "../Scene.h"
+#include <glm.hpp>
+#include <GL/glew.h>
+#include "../../Items/Teapot.h"
+#include "../../Items/Plane.h"
+
+class SimpleParticles : public Scene
+{
+public:
+	SimpleParticles();
+	void ProcessInput(int key, int action) override;
+	void InitScene() override;
+	void Update(float t) override;
+	void Render() override;
+	void Shutdown() override;
+	void Resize(int x, int y) override;
+private:
+	ShaderProgram _shader;
+	GLuint _initVel, _startTime, _particles;
+	GLuint _nParticles;
+	Plane* _plane;
+	glm::mat4 _model;
+	glm::mat4 _view;
+	glm::mat4 _projection;
+	glm::vec4 _lightPos;
+	GLuint _vao;
+	float _angle;
+	float _time;
+	int _width;
+	int _height;
+
+	void SetMatrices();
+	void CompileAndLinkShader();
+
+	void InitBuffers();
+	float RandFloat();
+};
